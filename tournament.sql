4953dbcd0e56db69a050c88babef6723ddc40c3d
Dee Reddy <d.reddy@yandex.com>
2015-10-28 10:11:59 -0400

    style: Format sql views

--- tournament.sql
+++ tournament.sql
@@ -56,40 +56,60 @@ CREATE TABLE matches(match_id SERIAL PRIMARY KEY,
 -- Views used for player_standings:
 
 -- Find number of matches played per player:
-CREATE OR REPLACE VIEW num_matches AS
-    SELECT players.id, COALESCE(COUNT(players.id), 0)
-    AS games_played
-    FROM players, matches
-    WHERE (players.id = winner_id OR players.id = loser_id)
+CREATE
+    OR REPLACE VIEW num_matches AS
+
+SELECT players.id
+    ,COALESCE(COUNT(players.id), 0) AS games_played
+FROM players
+    ,matches
+WHERE (
+        players.id = winner_id
+        OR players.id = loser_id
+        )
     AND matches.tournament_id = tournament_id
-    GROUP BY players.id
-    ORDER BY games_played DESC;
+GROUP BY players.id
+ORDER BY games_played DESC;
+
 
 -- Find number of matches won per player:
-CREATE OR REPLACE VIEW games_won AS
-    SELECT players.id as id, players.name,
-    COUNT(subQuery.winner_id) AS wins
-    FROM players LEFT JOIN (SELECT winner_id
-                         FROM matches
-                         WHERE matches.tournament_id = tournament_id)
-                         AS subQuery
-    ON players.id = subQuery.winner_id
-    GROUP BY players.id;
+CREATE
+    OR REPLACE VIEW games_won AS
+
+SELECT players.id AS id
+    ,players.NAME
+    ,COUNT(subQuery.winner_id) AS wins
+FROM players
+LEFT JOIN (
+    SELECT winner_id
+    FROM matches
+    WHERE matches.tournament_id = tournament_id
+    ) AS subQuery ON players.id = subQuery.winner_id
+GROUP BY players.id;
 
 
 -- Find opponent match wins (omw):
 -- (subquery creates 2 columns of id & every opponent he has played)
-CREATE OR REPLACE VIEW omw AS
-    SELECT x, CAST(SUM(wins) AS INTEGER) AS opponent_wins
-    FROM games_won, (SELECT winner_id AS x, loser_id
-                  AS opponent FROM matches
-                  WHERE matches.tournament_id = tournament_id
-                  UNION
-                  SELECT loser_id AS x, winner_id
-                  AS opponent FROM matches
-                  WHERE matches.tournament_id = tournament_id
-                  ORDER BY x ASC)
-                  AS subQuery
-    WHERE id = opponent
-    GROUP BY x
-    ORDER BY x;
+CREATE
+    OR REPLACE VIEW omw AS
+
+SELECT x
+    ,CAST(SUM(wins) AS INT) AS opponent_wins
+FROM games_won
+    ,(
+        SELECT winner_id AS x
+            ,loser_id AS opponent
+        FROM matches
+        WHERE matches.tournament_id = tournament_id
+        
+        UNION
+        
+        SELECT loser_id AS x
+            ,winner_id AS opponent
+        FROM matches
+        WHERE matches.tournament_id = tournament_id
+        ORDER BY x ASC
+        ) AS subQuery
+WHERE id = opponent
+GROUP BY x
+ORDER BY x;
